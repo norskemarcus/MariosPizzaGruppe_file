918a61b7b9b835207a35e58e17ae913930eb337b
norskemarcus <marc791j@stud.kea.dk>
2022-04-20 08:17:59 +0200

    Test marcus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Test");
         System.out.println("test2 igen");
+        System.out.println("Test fra Marcus");
 
 
     }
