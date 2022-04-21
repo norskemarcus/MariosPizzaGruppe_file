b98eac42eb08ef5ffa53d3c870d23f3aa2b4660d
ferh0162 <ferh0162@stud.kea.dk>
2022-04-21 10:17:58 +0200

    Added CustomerOrder class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Test");
-        System.out.println("test2 igen");
-        System.out.println("Test fra Marcus");
 
 
 
--- src/com/company/UI.java
+++ src/com/company/UI.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class UI {
 
   Pizza pizza1 = new Pizza(1, "Vesuvio", "tomatsauce", "ost", "skinke", "oregano", 57);
   Pizza pizza2 = new Pizza(2, "Amerikaner", "tomatsauce", "ost", "oksefars", "oregano", 53);
   Pizza pizza3 = new Pizza (3, "Cacciatore", "tomatsauce", "ost", "pepperoni", "oregano", 57);
   // Pizza pizza4 = new Pizza (4, "Carbona", )
+
+  ArrayList<Pizza> pizzas = new ArrayList<>();
 
 }
