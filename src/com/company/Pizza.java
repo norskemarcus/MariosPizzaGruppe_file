bc8a537a483ffd1948796ae59034e474d03b7556
norskemarcus <marc791j@stud.kea.dk>
2022-04-21 10:18:53 +0200

    changed ingrediens to description

--- src/com/company/Pizza.java
+++ src/com/company/Pizza.java
@@ -4,56 +4,17 @@ public class Pizza {
 
   private int number;
   private String name;
-  private String ingredient1;
-  private String ingredient2;
-  private String ingredient3;
-  private String ingredient4;
-  private String ingredient5;
-  private String ingredient6;
-  private String ingredient7;
+  private String description;
   private int price;
 
-  public Pizza(int number, String name, String ingredient1, String ingredient2, String ingredient3, String ingredient4, int price) {
+  public Pizza(int number, String name, String description, int price) {
+    this.number = number;
     this.name = name;
-    this.ingredient1 = ingredient1;
-    this.ingredient2 = ingredient2;
-    this.ingredient3 = ingredient3;
-    this.ingredient4 = ingredient4;
     this.price = price;
+    this.description = description;
   }
 
-  public Pizza(int number, String name, String ingredient1, String ingredient2, String ingredient3, String ingredient4, String ingredient5, int price) {
-    this.name = name;
-    this.ingredient1 = ingredient1;
-    this.ingredient2 = ingredient2;
-    this.ingredient3 = ingredient3;
-    this.ingredient4 = ingredient4;
-    this.ingredient5 = ingredient5;
-    this.price = price;
-  }
 
-  public Pizza(int number, String name, String ingredient1, String ingredient2, String ingredient3, String ingredient4, String ingredient5, String ingredient6, int price) {
-    this.name = name;
-    this.ingredient1 = ingredient1;
-    this.ingredient2 = ingredient2;
-    this.ingredient3 = ingredient3;
-    this.ingredient4 = ingredient4;
-    this.ingredient5 = ingredient5;
-    this.ingredient6 = ingredient6;
-    this.price = price;
-  }
-
-  public Pizza(int number, String name, String ingredient1, String ingredient2, String ingredient3, String ingredient4, String ingredient5, String ingredient6, String ingredient7, int price) {
-    this.name = name;
-    this.ingredient1 = ingredient1;
-    this.ingredient2 = ingredient2;
-    this.ingredient3 = ingredient3;
-    this.ingredient4 = ingredient4;
-    this.ingredient5 = ingredient5;
-    this.ingredient6 = ingredient6;
-    this.ingredient7 = ingredient7;
-    this.price = price;
-  }
 
   public String getName() {
     return name;
